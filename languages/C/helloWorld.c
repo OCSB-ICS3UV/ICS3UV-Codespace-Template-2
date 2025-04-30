85331850dcbc797d35a505ad346a42102017787e
Patrick Coxall <mr.coxall@mths.ca>
2025-04-30 18:56:40 +0000

    update

---
+++ languages/C/helloWorld.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This is the Hello, World! program
+
+#include <stdio.h>
+
+int main() {
+    // This function prints "Hello, World!" to the console
+    printf("Hello, World!\n");
+
+    printf("\nDone.\n");
+    return 0;
+}
